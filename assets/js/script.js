153e5853ee958cc862f7040b6bf9098bd435305f
Loresh Kumar Dash <dashloreshkumar@gmail.com>
2025-11-25 10:20:15 +0530

    clear

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const heroCarousel = document.querySelector("#heroCarousel");
-  new bootstrap.Carousel(heroCarousel, {
-    interval: 5000,
-    ride: "carousel",
-    pause: false,
-    wrap: true,
-  });
-});
